a43e8e2de6d9250bce3961ee0ef0c7837832eff4
DavidOliverius <99708980+DavidOliverius@users.noreply.github.com>
2023-02-28 11:51:25 +1100

    Sorting functionality

--- src/pages/m-workshop/m-workshoplist.jsx
+++ src/pages/m-workshop/m-workshoplist.jsx
@@ -40,7 +40,7 @@ const FilterList = ({ workshops, setFilteredWorkshops }) => {
       ) {
         accordionRef.current
           .querySelector('.MuiButtonBase-root.Mui-expanded')
-          .click();
+          ?.click();
       }
     };
     document.addEventListener('mousedown', handleClick);
@@ -129,6 +129,35 @@ const FilterList = ({ workshops, setFilteredWorkshops }) => {
 
 const WorkshopList = () => {
   const [filteredWorkshops, setFilteredWorkshops] = useState(workshops);
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: null });
+
+  const handleSort = (key) => {
+    let direction = 'ascending';
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+  };
+
+  const [sortedWorkshops, setSortedWorkshops] = useState([]);
+
+  useEffect(() => {
+    const copyOfWorkshops = [...filteredWorkshops];
+
+    if (sortConfig.key !== null) {
+      copyOfWorkshops.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+
+    setSortedWorkshops(copyOfWorkshops);
+  }, [sortConfig, filteredWorkshops]);
 
   return (
     <div style={{ backgroundColor: '#c9cdd6' }}>
@@ -137,7 +166,37 @@ const WorkshopList = () => {
         setFilteredWorkshops={setFilteredWorkshops}
       />
       <div style={{ display: 'flex', flexDirection: 'column' }}>
-        {filteredWorkshops.map((workshop) => (
+        <div style={{ display: 'flex', marginBottom: '8px' }}>
+          <Button
+            variant="outlined"
+            onClick={() => handleSort('date')}
+            style={{ marginRight: '8px' }}
+          >
+            Date
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => handleSort('workshop')}
+            style={{ marginRight: '8px' }}
+          >
+            Workshop
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => handleSort('participants')}
+            style={{ marginRight: '8px' }}
+          >
+            Participants
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => handleSort('progress')}
+            style={{ marginRight: '8px' }}
+          >
+            Progress
+          </Button>
+        </div>
+        {sortedWorkshops.map((workshop) => (
           <WorkshopCard
             key={workshop.id}
             workshop={workshop.workshop}
