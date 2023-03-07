b1827cf8905a49c5a99e08a0fa17583f03299018
DavidOliverius <99708980+DavidOliverius@users.noreply.github.com>
2023-03-07 18:16:22 +1100

    Added storybook viewports
    Set default view as iPhone

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,9 +1,17 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+console.log(INITIAL_VIEWPORTS);
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  viewport: {
+    viewports: INITIAL_VIEWPORTS, // newViewports would be an ViewportMap. (see below for examples)
+    defaultViewport: 'iphone12',
+  },
+};
